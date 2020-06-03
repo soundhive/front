5928bebeffec2d44e109e16169615f8d4082f729
Stéphane <rabena.stephane@gmail.com>
2020-06-03 15:00:19 +0200

    feat(sidebar): add links in sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  homeLink = '/home';
+  subscriptionsLink = '/subscriptions';
+  chartsLink = '/charts';
+  discoverLink = '/discover';
+  mySongsLink = '/my-songs';
+  myAlbumsLink = '/my-albums';
+  myActivityLink = '/my-activity';
 
   constructor() { }
 
