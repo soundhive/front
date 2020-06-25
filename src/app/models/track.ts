d32379cc51ec7e6447d6861772a9d12e71184f55
Stanislas Lange <stanislas.lange@pm.me>
2020-06-25 15:36:59 +0200

    Add models folder and Track model

---
+++ src/app/models/track.ts
@@ -0,0 +1,14 @@
+import { User } from './user';
+
+export class Track {
+  id: string;
+  title: string;
+  description: string;
+  genre: string;
+  filename: string;
+  // listenings: Listening[];
+  createdAt: Date;
+  updatedAt: Date;
+  // album: Album;
+  // user: User;
+}
