6b79682dc60ace9e9af54b94e63e09d6b680e200
Stanislas Lange <stanislas.lange@pm.me>
2020-06-25 15:39:02 +0200

    Update model import

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { User } from './user';
+import { User } from '../models/user';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
