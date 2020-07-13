b83ac0e60f9b46a396f2269f74d82926c9fb2ff8
Stanislas Lange <stanislas.lange@pm.me>
2020-07-13 20:23:27 +0200

    Add S3 env for staging and prod

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   api_endpoint: 'https://soundhive-api-staging.herokuapp.com',
+  s3_bucket: 'soundhive',
+  s3_endpoint: 'https://s3.fr-par.scw.cloud',
 };
