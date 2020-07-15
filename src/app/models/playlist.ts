075422999e0bee7474219cb5bd6590cb1ac0d8bf
Stéphane <rabena.stephane@gmail.com>
2020-07-16 01:44:03 +0200

    Move playlist.ts into models

---
+++ src/app/models/playlist.ts
@@ -0,0 +1,10 @@
+export class Playlist {
+  id: string;
+  title: string;
+  description?: string;
+  coverFilename: string;
+  createdAt: Date;
+  updatedAt: Date;
+  // tracks: Track[];
+  // user: User;
+}
--- src/app/playlist.ts
+++
@@ -1,4 +0,0 @@
-export class Playlist {
-  id: number;
-  title: string;
-}
