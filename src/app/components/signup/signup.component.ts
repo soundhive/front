0a921ab5beca5b4723d2a51feb4309cebc3487d6
Stanislas Lange <stanislas.lange@pm.me>
2020-07-18 18:30:46 +0200

    Signup: add basic client-side validation

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { AuthService } from './../../shared/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from './../../shared/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -18,10 +18,10 @@ export class SignupComponent implements OnInit {
     public router: Router,
   ) {
     this.signupForm = this.fb.group({
-      name: [''],
-      username: [''],
-      email: [''],
-      password: [''],
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
     });
   }
 
