e6c6c8c2db851ea53d1b63f620137bac0d86a08d
Stanislas Lange <stanislas.lange@pm.me>
2020-07-18 14:55:14 +0200

    Cleanup resolves

--- src/app/resolves/user-albums.resolve.ts
+++ src/app/resolves/user-albums.resolve.ts
@@ -3,16 +3,12 @@ import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Album } from 'src/app/models/album';
 import { UserService } from 'src/app/services/user.service';
-import { AuthService } from 'src/app/shared/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserAlbumsResolve implements Resolve<Album[]> {
-  constructor(
-    private userService: UserService,
-    private authService: AuthService,
-  ) {}
+  constructor(private userService: UserService) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<Album[]> {
     return this.userService.getUserAlbums(route.params.username);
--- src/app/resolves/user-history.resolve.ts
+++ src/app/resolves/user-history.resolve.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
-
-import { User } from 'src/app/models/user';
-import { UserService } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/shared/auth.service';
-import { Track } from 'src/app/models/track';
 import { Listening } from '../models/listening';
-import { HistoryService } from '../services/history.service';
+import { Pagination } from '../models/pagination/pagination';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserHistoryResolve implements Resolve<{ items: Listening[] }> {
+export class UserHistoryResolve implements Resolve<Pagination<Listening>> {
   constructor(
-    private historyService: HistoryService,
+    private userService: UserService,
     private authService: AuthService,
   ) {}
 
-  resolve(route: ActivatedRouteSnapshot): Observable<{ items: Listening[] }> {
-    return this.historyService.getForUser(this.authService.username);
+  resolve(): Observable<Pagination<Listening>> {
+    return this.userService.getTracksHistory(this.authService.username);
   }
 }
