825baadab0f7a9945bd479ff099ede7bbd2728df
Stanislas Lange <stanislas.lange@pm.me>
2020-07-19 14:33:12 +0200

    Unsubscibre on destroy when necessary

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Track } from 'src/app/models/track';
 import { PlayerService } from 'src/app/services/player.service';
 import { TrackService } from 'src/app/services/track.service';
@@ -9,11 +10,14 @@ import { environment } from 'src/environments/environment';
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss'],
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent implements OnInit, OnDestroy {
   s3Bucket: string;
   s3Endpoint: string;
 
   track: Track = null;
+
+  playTrackSub: Subscription;
+  loadTrackSub: Subscription;
 
   constructor(
     public playerService: PlayerService,
@@ -47,39 +51,48 @@ export class PlayerComponent implements OnInit {
     });
 
     // User clicks on play button
-    this.playerService.playTrackEvent.subscribe((track: Track) => {
-      localStorage.setItem('current_track', track.id);
+    this.playTrackSub = this.playerService.playTrackEvent.subscribe(
+      (track: Track) => {
+        localStorage.setItem('current_track', track.id);
 
-      this.track = track;
+        this.track = track;
 
-      // Once the track has been loaded, play it
-      player.onloadeddata = () => {
-        player.play();
-      };
-    });
-
-    // User clicks on title
-    this.playerService.loadTrackEvent.subscribe((track: Track) => {
-      localStorage.setItem('current_track', track.id);
-
-      const currentlyPlaying = !player.pause;
-
-      this.track = track;
-
-      // If not track was playing, we only load the track into the player
-      if (currentlyPlaying) {
         // Once the track has been loaded, play it
         player.onloadeddata = () => {
           player.play();
         };
-      }
-    });
+      },
+    );
+
+    // User clicks on title
+    this.loadTrackSub = this.playerService.loadTrackEvent.subscribe(
+      (track: Track) => {
+        localStorage.setItem('current_track', track.id);
+
+        const currentlyPlaying = !player.pause;
+
+        this.track = track;
+
+        // If not track was playing, we only load the track into the player
+        if (currentlyPlaying) {
+          // Once the track has been loaded, play it
+          player.onloadeddata = () => {
+            player.play();
+          };
+        }
+      },
+    );
   }
 
   onEnded() {
     // This event is not reliable!
     // https://github.com/vmudigal/ngx-audio-player/issues/66
     // See the `pause` eventListener above instead.
+  }
+
+  ngOnDestroy() {
+    this.playTrackSub.unsubscribe();
+    this.loadTrackSub.unsubscribe();
   }
 
   favoriteTrack() {
