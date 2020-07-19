52918998021c7e6eceef79433e5e4e8ebce5b6ab
Stéphane <rabena.stephane@gmail.com>
2020-07-19 20:55:36 +0200

    Display form on edit page (wip)

--- src/app/components/playlist-form/playlist-form.component.ts
+++ src/app/components/playlist-form/playlist-form.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PlaylistsService } from '../../services/playlists.service';
 import { AlertService } from '../../services/alert.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { requiredFileType } from '../../shared/required-file-type';
+import { Playlist } from '../../models/playlist';
 
 @Component({
   selector: 'app-new-playlist',
@@ -11,18 +12,21 @@ import { requiredFileType } from '../../shared/required-file-type';
   styleUrls: ['./playlist-form.component.scss'],
 })
 export class PlaylistFormComponent implements OnInit {
+  playlist: Playlist;
   playlistsForm: FormGroup;
   requiredFieldAlert = 'This field is required';
   options = {
     autoClose: true,
     keepAfterRouteChange: false,
   };
+  isNewPlaylistForm = true;
 
   constructor(
     private formBuilder: FormBuilder,
     private playlistsService: PlaylistsService,
     public alertService: AlertService,
     public router: Router,
+    private route: ActivatedRoute,
   ) {}
 
   ngOnInit(): void {
@@ -30,14 +34,36 @@ export class PlaylistFormComponent implements OnInit {
   }
 
   initPlaylistsForm() {
-    this.playlistsForm = this.formBuilder.group({
-      title: ['', Validators.required],
-      description: ['', [Validators.required, Validators.maxLength(500)]],
-      coverImageFile: [
-        null,
-        [Validators.required, requiredFileType(['png', 'jpg', 'jpeg'])],
-      ],
-    });
+    if (this.route.snapshot.url[2].path === 'edit') {
+      this.isNewPlaylistForm = false;
+
+      const id = this.route.snapshot.paramMap.get('id');
+
+      this.playlistsService.getPlaylist(id).subscribe((res: Playlist) => {
+        this.playlist = res;
+      });
+
+      this.playlistsForm = this.formBuilder.group({
+        title: [this.playlist.title, Validators.required],
+        description: [
+          this.playlist.description,
+          [Validators.required, Validators.maxLength(500)],
+        ],
+        coverImageFile: [
+          null,
+          [Validators.required, requiredFileType(['png', 'jpg', 'jpeg'])],
+        ],
+      });
+    } else {
+      this.playlistsForm = this.formBuilder.group({
+        title: ['', Validators.required],
+        description: ['', [Validators.required, Validators.maxLength(500)]],
+        coverImageFile: [
+          null,
+          [Validators.required, requiredFileType(['png', 'jpg', 'jpeg'])],
+        ],
+      });
+    }
   }
 
   isFormValid() {
@@ -56,20 +82,39 @@ export class PlaylistFormComponent implements OnInit {
     formData.append('cover_file', submittedCoverFile);
     formData.append('tracks', '[]');
 
-    this.playlistsService.createPlaylist(formData).subscribe(
-      (res) => {
-        if (res) {
-          this.playlistsService.addPlaylistToSidebar(res);
-          this.router.navigate(['/playlists/', res.id]);
-          this.alertService.success(
-            'New playlist successfully created!',
-            this.options,
-          );
-        }
-      },
-      (err) => {
-        this.alertService.error(err.message, this.options);
-      },
-    );
+    if (this.isNewPlaylistForm) {
+      this.playlistsService.createPlaylist(formData).subscribe(
+        (res) => {
+          if (res) {
+            this.playlistsService.addPlaylistToSidebar(res);
+            this.router.navigate(['/playlists/', res.id]);
+            this.alertService.success(
+              'New playlist successfully created!',
+              this.options,
+            );
+          }
+        },
+        (err) => {
+          this.alertService.error(err.message, this.options);
+        },
+      );
+    } else {
+      this.playlistsService
+        .updatePlaylist(formData, this.playlist.id)
+        .subscribe(
+          (res) => {
+            if (res) {
+              this.router.navigate(['/playlists/', res.id]);
+              this.alertService.success(
+                'This playlist has been successfully updated!',
+                this.options,
+              );
+            }
+          },
+          (err) => {
+            this.alertService.error(err.message, this.options);
+          },
+        );
+    }
   }
 }
