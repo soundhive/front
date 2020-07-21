87b421180abd3c3b22af2868b240cd568c954b97
Stanislas Lange <stanislas.lange@pm.me>
2020-07-21 23:53:00 +0200

    DownloadComponent: fix formating

--- src/app/components/download/download.component.ts
+++ src/app/components/download/download.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-download',
   templateUrl: './download.component.html',
-  styleUrls: ['./download.component.scss']
+  styleUrls: ['./download.component.scss'],
 })
 export class DownloadComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
